c6e26e32dbc325f344526e2565c923c5c29a55a9
HusseinMEHussein <husseinmehussein@gmail.com>
2025-05-30 04:28:48 +0000

    pages deleted

--- pages/1_pg1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write("# Profile")
-st.write("## This is a title H2 Title in page profile!")
-
-
--- pages/2_pg2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write("# dfgdsg h")
-st.write("## This is a title H2 Title in page profile!")
-
-
